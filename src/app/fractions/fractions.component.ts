a730455fd2f8239cdae7b0ea948b0cfeb7692d26
matthewberes <matthewberes@trentu.ca>
2024-09-26 21:42:10 -0400

    skeleton for other fraction operators

--- src/app/fractions/fractions.component.ts
+++ src/app/fractions/fractions.component.ts
@@ -21,21 +21,46 @@ export class FractionsComponent {
   })
 
   calculate() {
+    this.calculation = "";
     this.showCalculation = false;
+    // Add/Subtract
+    let commonDenominator: number;
+    let num1Large: number;
+    let num2Large: number;
+    // Multiply/Divide
+    let numTotal: number;
+    let denomTotal: number;
+    let whole: number;
     switch (this.operator.nativeElement.value) {
       case "add":
-        let commonDenominator = this.fractionsForm.get("denominator1")?.value * this.fractionsForm.get("denominator2")?.value
-        let num1Large = this.fractionsForm.get("numerator1")?.value * this.fractionsForm.get("denominator2")?.value;
-        let num2Large = this.fractionsForm.get("numerator2")?.value * this.fractionsForm.get("denominator1")?.value;
-        let whole = (num1Large + num2Large) / commonDenominator;
+        commonDenominator = this.fractionsForm.get("denominator1")?.value * this.fractionsForm.get("denominator2")?.value
+        num1Large = this.fractionsForm.get("numerator1")?.value * this.fractionsForm.get("denominator2")?.value;
+        num2Large = this.fractionsForm.get("numerator2")?.value * this.fractionsForm.get("denominator1")?.value;
+        whole = (num1Large + num2Large) / commonDenominator;
         this.calculation = String(whole) + " | " + (num1Large + num2Large) + " / " + commonDenominator
         this.showCalculation = true;
         break;
       case "subtract":
+        commonDenominator = this.fractionsForm.get("denominator1")?.value * this.fractionsForm.get("denominator2")?.value
+        num1Large = this.fractionsForm.get("numerator1")?.value * this.fractionsForm.get("denominator2")?.value;
+        num2Large = this.fractionsForm.get("numerator2")?.value * this.fractionsForm.get("denominator1")?.value;
+        whole = (num1Large - num2Large) / commonDenominator;
+        this.calculation = String(whole) + " | " + (num1Large - num2Large) + " / " + commonDenominator
+        this.showCalculation = true;
         break;
       case "multiply":
+        numTotal = this.fractionsForm.get("numerator1")?.value * this.fractionsForm.get("numerator2")?.value;
+        denomTotal = this.fractionsForm.get("denominator1")?.value * this.fractionsForm.get("denominator2")?.value;
+        whole = numTotal / denomTotal;
+        this.calculation = String(whole) + " | " + numTotal + " / " + denomTotal
+        this.showCalculation = true;
         break;
       case "divide":
+        numTotal = this.fractionsForm.get("numerator1")?.value / this.fractionsForm.get("numerator2")?.value;
+        denomTotal = this.fractionsForm.get("denominator1")?.value / this.fractionsForm.get("denominator2")?.value;
+        whole = numTotal / denomTotal;
+        this.calculation = String(whole) + " | " + numTotal + " / " + denomTotal
+        this.showCalculation = true;
         break;
     }
   }
