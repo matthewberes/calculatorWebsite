4905dcf371492ab50a6e8ecb8c41567a43a39559
matthewberes <matthewberes@trentu.ca>
2024-09-27 23:28:29 -0400

    simplify result

--- src/app/fractions/fractions.component.ts
+++ src/app/fractions/fractions.component.ts
@@ -11,7 +11,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class FractionsComponent {
   showCalculation: boolean = false;
-  calculation: string = ""
+  calculation: string = "";
   @ViewChild('operator') operator!: ElementRef;
   fractionsForm = new FormGroup({
     numerator1: new FormControl(),
@@ -23,6 +23,8 @@ export class FractionsComponent {
   calculate() {
     this.calculation = "";
     this.showCalculation = false;
+    let whole: number;
+    let gcf: number;
     // Add/Subtract
     let commonDenominator: number;
     let num1Large: number;
@@ -30,38 +32,52 @@ export class FractionsComponent {
     // Multiply/Divide
     let numTotal: number;
     let denomTotal: number;
-    let whole: number;
+
     switch (this.operator.nativeElement.value) {
       case "add":
-        commonDenominator = this.fractionsForm.get("denominator1")?.value * this.fractionsForm.get("denominator2")?.value
+        commonDenominator = this.fractionsForm.get("denominator1")?.value * this.fractionsForm.get("denominator2")?.value;
         num1Large = this.fractionsForm.get("numerator1")?.value * this.fractionsForm.get("denominator2")?.value;
         num2Large = this.fractionsForm.get("numerator2")?.value * this.fractionsForm.get("denominator1")?.value;
-        whole = (num1Large + num2Large) / commonDenominator;
-        this.calculation = String(whole) + " | " + (num1Large + num2Large) + " / " + commonDenominator
+        let sum = num1Large + num2Large;
+        whole = sum / commonDenominator;
+        gcf = this.gcf(sum, commonDenominator);
+        this.calculation = String(whole) + " | " + (sum / gcf) + " / " + (commonDenominator / gcf);
         this.showCalculation = true;
         break;
       case "subtract":
-        commonDenominator = this.fractionsForm.get("denominator1")?.value * this.fractionsForm.get("denominator2")?.value
+        commonDenominator = this.fractionsForm.get("denominator1")?.value * this.fractionsForm.get("denominator2")?.value;
         num1Large = this.fractionsForm.get("numerator1")?.value * this.fractionsForm.get("denominator2")?.value;
         num2Large = this.fractionsForm.get("numerator2")?.value * this.fractionsForm.get("denominator1")?.value;
-        whole = (num1Large - num2Large) / commonDenominator;
-        this.calculation = String(whole) + " | " + (num1Large - num2Large) + " / " + commonDenominator
+        let difference = num1Large - num2Large;
+        whole = difference / commonDenominator;
+        gcf = this.gcf(difference, commonDenominator);
+        this.calculation = String(whole) + " | " + (difference / gcf) + " / " + (commonDenominator / gcf);
         this.showCalculation = true;
         break;
       case "multiply":
         numTotal = this.fractionsForm.get("numerator1")?.value * this.fractionsForm.get("numerator2")?.value;
         denomTotal = this.fractionsForm.get("denominator1")?.value * this.fractionsForm.get("denominator2")?.value;
         whole = numTotal / denomTotal;
-        this.calculation = String(whole) + " | " + numTotal + " / " + denomTotal
+        gcf = this.gcf(numTotal, denomTotal);
+        this.calculation = String(whole) + " | " + (numTotal / gcf) + " / " + (denomTotal / gcf);
         this.showCalculation = true;
         break;
       case "divide":
         numTotal = this.fractionsForm.get("numerator1")?.value / this.fractionsForm.get("numerator2")?.value;
         denomTotal = this.fractionsForm.get("denominator1")?.value / this.fractionsForm.get("denominator2")?.value;
         whole = numTotal / denomTotal;
-        this.calculation = String(whole) + " | " + numTotal + " / " + denomTotal
+        gcf = this.gcf(numTotal, denomTotal);
+        this.calculation = String(whole) + " | " + (numTotal / gcf) + " / " + (denomTotal / gcf);
         this.showCalculation = true;
         break;
+    }
+  }
+
+  gcf(a: number, b: number): number {
+    if (b) {
+      return this.gcf(b, a % b);
+    } else {
+      return Math.abs(a);
     }
   }
 }
