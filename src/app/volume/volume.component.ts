c98d3693b92b5fc7549e092937e43c4873824aff
matthewberes <matthewberes@trentu.ca>
2024-10-10 23:56:24 -0400

    more volume calculations

--- src/app/volume/volume.component.ts
+++ src/app/volume/volume.component.ts
@@ -14,14 +14,34 @@ export class VolumeComponent implements OnInit {
   showCalculation: boolean = false;
   currShape: string = "";
   @ViewChild('shape') shape!: ElementRef;
-  prismForm = new FormGroup({
+  @ViewChild('circleMeasurementType') circleMeasurementType!: ElementRef;
+  @ViewChild('sphereMeasurementType') sphereMeasurementType!: ElementRef;
+
+  rectangularPrismForm = new FormGroup({
     length: new FormControl(),
     width: new FormControl(),
-    height: new FormControl(),
+    height: new FormControl()
+  })
+  circularForm = new FormGroup({
+    circleValue: new FormControl(),
+    height: new FormControl()
+  })
+  gonalPrismForm = new FormGroup({
+    length: new FormControl(),
+    height: new FormControl()
+  })
+  sphereForm = new FormGroup({
+    circleValue: new FormControl()
+  })
+  triangularPrismForm = new FormGroup({
+    side1: new FormControl(),
+    side2: new FormControl(),
+    side3: new FormControl(),
+    height: new FormControl()
   })
 
   ngOnInit(): void {
-    this.currShape = 'rectangle';
+    this.currShape = 'cone';
   }
 
   selectChange() {
@@ -32,35 +52,81 @@ export class VolumeComponent implements OnInit {
 
   calculate() {
     this.showCalculation = false;
+    let base: number
     switch (this.currShape) {
       case "cone":
+        if (this.circleMeasurementType.nativeElement.value == "radius") {
+          base = Math.pow(this.circularForm.get("circleValue")?.value, 2) * Math.PI;
+        } else {
+          base = Math.pow(this.circularForm.get("circleValue")?.value / 2, 2) * Math.PI;
+        }
+        this.calculation = String(base * (this.circularForm.get("height")?.value / 3));
+        this.showCalculation = true;
         break;
       case "cylinder":
+        if (this.circleMeasurementType.nativeElement.value == "radius") {
+          base = Math.pow(this.circularForm.get("circleValue")?.value, 2) * Math.PI;
+        } else {
+          base = Math.pow(this.circularForm.get("circleValue")?.value / 2, 2) * Math.PI;
+        }
+        this.calculation = String(base * this.circularForm.get("height")?.value);
+        this.showCalculation = true;
         break;
       case "dodecahedron":
         break;
       case "ellipsoid":
         break;
+      case "hexagonalPrism":
+        base = ((3 * Math.sqrt(3)) / 2) * (Math.pow(this.gonalPrismForm.get("length")?.value, 2));
+        this.calculation = String(base * this.gonalPrismForm.get("height")?.value);
+        this.showCalculation = true;
+        break;
       case "hexagonalPyramid":
         break;
       case "icosahedron":
+        break;
+      case "octagonalPrism":
+        base = (2 * (1 + Math.sqrt(2))) * (Math.pow(this.gonalPrismForm.get("length")?.value, 2));
+        this.calculation = String(base * this.gonalPrismForm.get("height")?.value);
+        this.showCalculation = true;
         break;
       case "octagonalPyramid":
         break;
       case "octahedron":
         break;
+      case "pentagonalPrism":
+        base = (((1 / 4) * Math.sqrt(5 * (5 + (2 * Math.sqrt(5)))))) * (Math.pow(this.gonalPrismForm.get("length")?.value, 2));
+        this.calculation = String(base * this.gonalPrismForm.get("height")?.value);
+        this.showCalculation = true;
+        break;
       case "pentagonalPyramid":
+        break;
+      case "pentagrammicPrism":
         break;
       case "pentagrammicPyramid":
         break;
-      case "prism":
-        let base = this.prismForm.get("length")?.value * this.prismForm.get("width")?.value;
-        this.calculation = String(base * this.prismForm.get("height")?.value);
+      case "rectangularPrism":
+        base = this.rectangularPrismForm.get("length")?.value * this.rectangularPrismForm.get("width")?.value;
+        this.calculation = String(base * this.rectangularPrismForm.get("height")?.value);
         this.showCalculation = true;
         break;
       case "semisphere":
+        if (this.sphereMeasurementType.nativeElement.value == "radius") {
+          base = Math.pow(this.sphereForm.get("circleValue")?.value, 3) * Math.PI;
+        } else {
+          base = Math.pow(this.sphereForm.get("circleValue")?.value / 2, 3) * Math.PI;
+        }
+        this.calculation = String((base * (4 / 3)) / 2);
+        this.showCalculation = true;
         break;
       case "sphere":
+        if (this.sphereMeasurementType.nativeElement.value == "radius") {
+          base = Math.pow(this.sphereForm.get("circleValue")?.value, 3) * Math.PI;
+        } else {
+          base = Math.pow(this.sphereForm.get("circleValue")?.value / 2, 3) * Math.PI;
+        }
+        this.calculation = String(base * (4 / 3));
+        this.showCalculation = true;
         break;
       case "squarePyramid":
         break;
@@ -68,12 +134,27 @@ export class VolumeComponent implements OnInit {
         break;
       case "torus":
         break;
+      case "triangularPrism":
+        this.calculation = String(((1 / 4) * this.triangularPrismForm.get("height")?.value) * Math.sqrt((-1 * Math.pow(this.triangularPrismForm.get("side1")?.value, 4)) + (2 * Math.pow(this.triangularPrismForm.get("side1")?.value * this.triangularPrismForm.get("side2")?.value, 2)) + (2 * Math.pow(this.triangularPrismForm.get("side1")?.value * this.triangularPrismForm.get("side3")?.value, 2)) - (Math.pow(this.triangularPrismForm.get("side2")?.value, 4)) + (2 * Math.pow(this.triangularPrismForm.get("side2")?.value * this.triangularPrismForm.get("side3")?.value, 2)) - Math.pow(this.triangularPrismForm.get("side3")?.value, 4)));
+        this.showCalculation = true;
+        break;
     }
   }
 
   clear() {
-    this.prismForm.get("length")?.setValue(null);
-    this.prismForm.get("width")?.setValue(null);
-    this.prismForm.get("height")?.setValue(null);
+    this.rectangularPrismForm.get("length")?.setValue(null);
+    this.rectangularPrismForm.get("width")?.setValue(null);
+    this.rectangularPrismForm.get("height")?.setValue(null);
+    this.circularForm.get("circleValue")?.setValue(null);
+    this.circularForm.get("height")?.setValue(null);
+    this.gonalPrismForm.get("length")?.setValue(null);
+    this.gonalPrismForm.get("height")?.setValue(null);
+    this.sphereForm.get("circleValue")?.setValue(null);
+    this.triangularPrismForm.get("side1")?.setValue(null);
+    this.triangularPrismForm.get("side2")?.setValue(null);
+    this.triangularPrismForm.get("side3")?.setValue(null);
+    this.triangularPrismForm.get("height")?.setValue(null);
+    this.calculation = "";
+    this.showCalculation = false;
   }
 }
