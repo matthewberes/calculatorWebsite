19756be41fb10232f588a119340198103635f2bd
matthewberes <matthewberes@trentu.ca>
2024-11-12 22:22:21 -0500

    begin implementing break-even point

--- src/app/break-even-point/break-even-point.component.ts
+++ src/app/break-even-point/break-even-point.component.ts
@@ -1,22 +1,36 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-break-even-point',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, ReactiveFormsModule],
   templateUrl: './break-even-point.component.html',
   styleUrl: './break-even-point.component.css'
 })
 export class BreakEvenPointComponent {
   calculation: string = "";
   showCalculation: boolean = false;
+  costForm = new FormGroup({
+    initial: new FormControl(),
+    cost: new FormControl()
+  })
+  profitForm = new FormGroup({
+    initial: new FormControl(),
+    cost: new FormControl()
+  })
 
   calculate() {
-
+    console.log(this)
   }
 
   clear() {
+    this.showCalculation = false;
+    this.profitForm.get('initial')?.setValue(null);
+    this.profitForm.get('cost')?.setValue(null);
+    this.costForm.get('initial')?.setValue(null);
+    this.costForm.get('cost')?.setValue(null);
 
   }
 }
